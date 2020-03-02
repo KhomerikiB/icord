4b7d34fa439b5606d065595dff7638e4b371bf8d
nikagog <ngogi15@freeuni.edu.ge>
2020-03-02 19:12:18 +0400

    wass

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -28,29 +28,42 @@ const Index = props => {
           </div>
           {isIndex ? (
             <div className="main-header__text__wrapper">
-              
-              <ReactFitText compressor={3.94}  minFontSize={10} maxFontSize = {50} >
-              <p className="main-header__text home-col-2">
-                <strong>XV INTERNATIONAL CONFERENCE ON RARE DISEASES AND
-                ORPHAN DRUGS</strong>
-               
-              </p>
-              </ReactFitText>
-             
+                <div className="firstGrid">
+                    
+                  <ReactFitText compressor={3.94}  minFontSize={10} maxFontSize = {50} >
+                  <p className="main-header__text home-col-2">
+                    <strong>XV INTERNATIONAL CONFERENCE ON RARE DISEASES AND
+                    ORPHAN DRUGS</strong>
+                  
+                  </p>
+                  </ReactFitText>
+                
 
 
-              <div className="main-header__text__bottom">
-              <ReactFitText compressor={1.4}  minFontSize={10} maxFontSize = {50} >
-               <strong> <p className="main-header__small__text">ICORD 2020</p> </strong>
-               </ReactFitText>
-               <div></div>
+                  <div className="main-header__text__bottom">
+                  <ReactFitText compressor={1.4}  minFontSize={10} maxFontSize = {50} >
+                  <strong> <p className="main-header__small__text">#ICORD2020</p> </strong>
+                  </ReactFitText>
+                  <div></div>
 
 
-               <ReactFitText compressor={2.5}  minFontSize={10} maxFontSize = {40} >
-               <strong> <p className="main-header__right__text">NOVEMBER 19-21, 2020 <br></br>  TBILISI,    GEORGIA</p>   </strong>
-               </ReactFitText>
+                  <ReactFitText compressor={2.5}  minFontSize={10} maxFontSize = {40} >
+                  <strong> <p ></p>   </strong>
+                  </ReactFitText>
+                  </div>
+
+              </div> 
+
+
+              <div className="SecondGrid">
+                
+              <strong><p className="main-header__right__text">NOVEMBER 19-21, 2020 <br></br>  TBILISI,    GEORGIA</p>   </strong>
               </div>
+              
+              
             </div>
+
+
           ) : (
             <div className="header-image">
               <img
@@ -58,6 +71,8 @@ const Index = props => {
                 src={require("../../static/images/header-image.jpg")}
               />
             </div>
+
+            
           )}
         </div>
       </div>
@@ -66,3 +81,28 @@ const Index = props => {
 };
 
 export default Index;
+
+
+
+
+// <ReactFitText compressor={3.94}  minFontSize={10} maxFontSize = {50} >
+//               <p className="main-header__text home-col-2">
+//                 <strong>XV INTERNATIONAL CONFERENCE ON RARE DISEASES AND
+//                 ORPHAN DRUGS</strong>
+               
+//               </p>
+//               </ReactFitText>
+             
+
+
+//               <div className="main-header__text__bottom">
+//               <ReactFitText compressor={1.4}  minFontSize={10} maxFontSize = {50} >
+//                <strong> <p className="main-header__small__text">#ICORD2020</p> </strong>
+//                </ReactFitText>
+//                <div></div>
+
+
+//                <ReactFitText compressor={2.5}  minFontSize={10} maxFontSize = {40} >
+//                <strong> <p className="main-header__right__text">NOVEMBER 19-21, 2020 <br></br>  TBILISI,    GEORGIA</p>   </strong>
+//                </ReactFitText>
+//               </div>
